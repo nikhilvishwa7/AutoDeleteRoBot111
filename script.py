e9d6e67d0b8c117c0e427d8c4a6179f49ee23e22
BitterBeast <141508197+BitterBeast@users.noreply.github.com>
2023-08-28 14:07:57 +0530

    Create script.py

---
+++ script.py
@@ -0,0 +1,5 @@
+class Script:
+  START_TXT="""Hai {},
+  I am Auto Delete Bot made by <a href=t.me/ZiB_BoTs>ZIB Bots</a>. I can delete incoming messages in a Specific Time Just Add Me to
+youre Group and Set Auto Delete Time by /autodel <time in minutes> and ready to go.
+Have any doubts ? ask at support group"""
