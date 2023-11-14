c3bbea9173d5b12091e1a93cde3e1bde83ae8d34
nikhilvishwa7 <123230101+nikhilvishwa7@users.noreply.github.com>
2023-11-14 11:31:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 from os import path, getenv
 
 class Config:
-    API_ID = int(getenv("API_ID", "0112234"))
-    API_HASH = getenv("API_HASH", "abcdefg")
-    BOT_TOKEN = getenv("BOT_TOKEN", "1234567891:AdDfgFRFVVfDEhdhyjjvjjftSEW")
+    API_ID = int(getenv("API_ID", ""))
+    API_HASH = getenv("API_HASH", "")
+    BOT_TOKEN = getenv("BOT_TOKEN", "")
     MONGO_URI = getenv("MONGO_URI", "")
     
 cfg = Config()
